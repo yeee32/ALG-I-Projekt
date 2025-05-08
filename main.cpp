e1c4e42006699eb43d5255c907e807075f2718d3
Vojtěch Ožana <97116766+yeee32@users.noreply.github.com>
2025-05-08 14:47:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,84 +5,165 @@
 #include <algorithm>
 #include <vector>
 #include <cmath>
+#include <chrono>
+#include <queue>
 
 using namespace std;
 
-// make a new mergeArrays functions!!
-vector<int> mergeArrays(vector<vector<int>>& arr){
-    vector<int> result;
+class MinHeapElement{
+    private:
+        int arrayIndex;
+        int elemIndex;
+        int value;
+    public:
+        MinHeapElement(int value, int arrayIndex, int elemIndex);
+        int getArrayIndex(){ 
+            return this->arrayIndex; 
+        };
 
-    for(const auto& v : arr){
-        for(int val : v){
-            result.push_back(val);
+        int getElemIndex(){ 
+            return this->elemIndex; 
+        };
+
+        int getValue(){ 
+            return this->value; 
+        };
+
+        bool operator<(const MinHeapElement& other) const{
+            return this->value > other.value;
+        }
+};
+
+MinHeapElement::MinHeapElement(int value, int arrayIndex, int elemIndex){
+    this->value = value;
+    this->arrayIndex = arrayIndex;
+    this->elemIndex = elemIndex;
+}
+
+vector<int> mergeKArrays(vector<vector<int>>& arrays){
+    vector<int> result;
+    priority_queue<MinHeapElement> minHeap;
+
+    for(int i = 0; i < arrays.size(); i++){
+        minHeap.push(MinHeapElement(arrays[i][0], i, 0)); // insert the first (smallest) element of an array into the minHeap
+    }
+
+    while(!minHeap.empty()){
+        MinHeapElement element = minHeap.top();
+        // what I need to remember:
+        // which array did the number came from (array index)
+        // what number was it (value)
+        // where was the number in the array (element index)
+        int value = element.getValue();
+        int arrayIndex = element.getArrayIndex();
+        int elemIndex = element.getElemIndex();
+
+        result.push_back(value);
+        minHeap.pop();
+        if(elemIndex + 1 < arrays[arrayIndex].size()){
+            int nextValue = arrays[arrayIndex][elemIndex++];
+            minHeap.push(MinHeapElement(nextValue, arrayIndex, elemIndex++));
         }
     }
-    sort(result.begin(), result.end());
     return result;
 }
 
+vector<vector<int>> browseFiles(int arrNum, string inputPath, int filesNum){
+    vector<vector<int>> finalArray;
+    int fileIndex = 0;
 
+    if(arrNum > filesNum){
+        cout << "input number is way too large!" << endl << "number of files in directory: " << filesNum << endl;
+        exit(1);
+    }
+
+    for(const auto& entry : filesystem::directory_iterator(inputPath)){
+        if(entry.is_regular_file()){
+            if(fileIndex >= arrNum){
+                break;
+            }
+            ifstream inputFile;
+            inputFile.open(entry.path());  
+
+            if(inputFile.fail()){
+                cout << "error while opening the file!" << endl;
+                exit(1);
+            }
+            vector<int> numberArr;
+            string line;
+            cout << entry << endl;
+            while(getline(inputFile, line)){
+                int number = stoi(line);
+                numberArr.push_back(number);
+            }   
+            finalArray.push_back(numberArr);
+            inputFile.close();
+        }
+        fileIndex++;
+    }
+    return finalArray;
+}
+
+void putToOutFile(string outputPath, vector<int> arr){
+    ofstream outputFile;
+    outputFile.open(outputPath);
+
+    if(outputFile.fail()){
+        cout << "error while writing to file!" << endl;
+        exit(1);
+    }    
+
+    for(int num : arr){
+        outputFile << num << endl;
+    }
+
+    outputFile.close();
+}
+
+// returns number of files in the folder
+int getFilesNum(string directory){
+    int filesNum = 0;
+    for(const auto& entry : filesystem::directory_iterator(directory)){
+        if(entry.is_regular_file()){
+            filesNum++;
+        }
+    }
+    return filesNum;
+}
+
+
+// need to add a file sorting function
 int main(int argc, char* argv[]){
+    auto startTime = chrono::high_resolution_clock::now();
+
     if(argc != 4){
         cout << "wrong number of arguments!" << endl;
         return 1;
     }
 
     int k = stoi(argv[1]); // number of lists to merge
-    string inputPath = argv[2]; // 
+    string inputPath = argv[2]; 
     string outputPath = argv[3];
-
     int num = 0;
-    int i = 0;
 
-    vector<vector<int>> arrays;
-
-    for(const auto& entry : filesystem::directory_iterator(inputPath)){
-        if(entry.is_regular_file()){
-            if(i >= k) break;
-            ifstream inputFile;
-            inputFile.open(entry.path());  
-
-            if(inputFile.fail()){
-                cout << "error while opening the file!" << endl;
-                return 1;
-            }
-            cout << entry.path() << endl;
-            vector<int> numberArr;
-            string line;
-
-            while(getline(inputFile, line)){
-                int number = stoi(line);
-                numberArr.push_back(number);
-            }   
-            arrays.push_back(numberArr);
-            inputFile.close();
-        }
-        i++;
+    // check if the input file exists
+    if(!filesystem::exists(inputPath) || !filesystem::is_directory(inputPath)){
+        cout << "input directory does not exist!" << endl;
+        exit(1);
     }
 
-    for (int i = 0; i < arrays.size(); i++) {
-        for (int num : arrays[i]) {
-            cout << num << " ";
-        }
-        cout << endl;
-        cout << "-- (" << i << ".txt) --" << endl;
-    }
+    int filesNum = getFilesNum(inputPath);
 
-    ofstream outputFile;
-    outputFile.open(outputPath);
 
-    if(outputFile.fail()){
-        cout << "error opening file!" << endl;
-        return 1;
-    }
+    vector<vector<int>> arrays = browseFiles(k, inputPath, filesNum);
+    vector<int> mergedArr = mergeKArrays(arrays);
+    putToOutFile(outputPath, mergedArr);
+    
+    auto endTime = chrono::high_resolution_clock::now();
 
-    vector<int> mergedArr = mergeArrays(arrays);
-    for(int v : mergedArr){
-        outputFile << v << endl;
-    }
+    auto elapsedTime = chrono::duration_cast<chrono::milliseconds>(endTime - startTime);
 
-    outputFile.close();
+    cout << "program took: " << elapsedTime.count() << " ms" << endl;
 
     return 0;
 }
